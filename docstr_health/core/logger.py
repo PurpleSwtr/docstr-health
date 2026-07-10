8241d8834c06a9b5fdebbd5a40e9ed467261dc52
PurpleSwtr <sergeenko.mihail@yandex.ru>
2026-07-10 11:08:37 +0300

    bug: fix .cache creating

--- docstr_health/core/logger.py
+++ docstr_health/core/logger.py
@@ -3,13 +3,15 @@ from logging.handlers import RotatingFileHandler
 
 from ..core.config import config
 
+config.get_logs_dir().mkdir(parents=True, exist_ok=True)
+
 handler = RotatingFileHandler(
     config.get_logs_dir() / "app.log", maxBytes=1000000, backupCount=5, encoding="utf-8"
 )
 
 console_handler = logging.StreamHandler()
 
-if config.parameters["debug"]:
+if config.parameters.get("debug", False):
     log_level = logging.DEBUG
 else:
     log_level = logging.INFO
